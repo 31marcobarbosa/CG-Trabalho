562d57339bb5d49859f3b6ec37ee418d04a7f21c
adrianasg122 <adriguedes@msn.com>
2017-02-27 18:13:56 +0000

    Corrigir o git

---
+++ Motor/motor3D.cpp
@@ -0,0 +1,93 @@
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+float size = 1;
+float angle = 1;
+
+void changeSize(int w, int h) {
+
+	// Prevent a divide by zero, when window is too short
+	// (you cant make a window with zero width).
+	if(h == 0)
+		h = 1;
+
+	// compute window's aspect ratio
+	float ratio = w * 1.0 / h;
+
+	// Set the projection matrix as current
+	glMatrixMode(GL_PROJECTION);
+	// Load Identity Matrix
+	glLoadIdentity();
+
+	// Set the viewport to be the entire window
+    glViewport(0, 0, w, h);
+
+	// Set perspective
+	gluPerspective(45.0f ,ratio, 1.0f ,1000.0f);
+
+	// return to the model view matrix mode
+	glMatrixMode(GL_MODELVIEW);
+}
+
+
+
+void renderScene(void) {
+
+	// clear buffers
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// set the camera
+	glLoadIdentity();
+	gluLookAt(sin(angle),5.0,5*cos(angle),
+		      0.0,0.0,0.0,
+			  0.0f,1.0f,0.0f);
+    angle +=0.01;
+
+// put drawing instructions here
+
+
+    glutWireTeapot(size);
+    //size += 0.005;
+
+
+	// End of frame
+	glutSwapBuffers();
+}
+
+
+
+
+int main(int argc, char **argv) {
+
+// put GLUT init here
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowPosition(100,100);
+    glutInitWindowSize(800,800);
+    glutCreateWindow("CG@DI");
+
+
+// put callback registration here
+
+    glutDisplayFunc(renderScene);
+    glutReshapeFunc(changeSize);
+    glutIdleFunc(renderScene);
+
+// OpenGL settings
+
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	glClearColor(0.0f,0.0f,0.0f,0.0f);
+
+// enter GLUT's main loop
+	glutMainLoop();
+
+	return 1;
+}
