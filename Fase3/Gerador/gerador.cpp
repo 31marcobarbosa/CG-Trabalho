f8904030767f690cbd1c6073890f3171384e0f27
ricardocerto16 <a75315@alunos.uminho.pt>
2017-04-08 01:15:22 +0100

    tentativa de por o gerador a funcionar com o path

--- Fase3/Gerador/gerador.cpp
+++ Fase3/Gerador/gerador.cpp
@@ -1,4 +1,18 @@
-#include "gerador.h"
+//#include "gerador.h"
+
+
+
+#define _USE_MATH_DEFINES
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <GL/glut.h>
+#include <stdlib.h>
+
+using namespace std;
 
 
  // meter as variaveis em ING
@@ -57,7 +71,8 @@ void cubo(double comp, double larg, double alt, int camadas, string nome){
 	/*
 	 * FACES DE CIMA E DE BAIXO
 	 */
-	y = alt / 2; yy = -y; //y é usado para indicar a altura da face de cima e yy é a altura a que a base de baixo se encontrará
+	y = alt / 2; 
+	yy = -y; //y é usado para indicar a altura da face de cima e yy é a altura a que a base de baixo se encontrará
 	z = larg / 2;		 //a variável z assume metade do valor da largura, ficando o poligono centrado no eixo do z
 
 	for (i = 0; i < camadas; i++){
@@ -370,6 +385,7 @@ void cilindro(float raio, float altura, int camadasH, int camadasV,string nome){
 	}
 
 }
+
 void menu() {
 
 	cout <<" ---------------------> MENU DE AJUDA <---------------------"<< endl;
@@ -410,9 +426,143 @@ void menu() {
 	cout <<"|                                                           |"<< endl;
 	cout <<"|                                                           |"<< endl;
 	cout <<" ------------------------------><---------------------------"<< endl;
-
-
 }
+
+float* formulaBezier( float tt, float *p1 , float *p2 , float *p3 , float *p4) {
+
+	float ite = 1.0 - tt;
+	float* ptt = new float[3];
+
+	float x0 , x1 , x2 , x3;
+	x0 = pow(ite,3);
+	x1 = 3 * pow(ite,2);
+	x2 = 3 * tt * tt * tt;
+	x3 = tt * tt * tt;
+
+	ppt[0] = x0*p1[0] + x1*p2[0] + x2*p3[0] + x3*p4[0];
+	ppt[1] = x0*p1[1] + x1*p2[1] + x2*p3[1] + x3*p4[1];
+	ppt[2] = x0*p1[2] + x1*p2[2] + x2*p3[2] + x3*p4[2];
+
+
+	return ppt;
+}
+
+
+float* bezier( float a , float b , int* indice , float** pontos , int ni , int np) {
+
+	float* ponto = new float[3];
+	float altp[4][3];
+	float res[4][3];
+	int i , j = 0 , x = 0;
+	float *calculo;
+
+
+	for( i = 0 ; i < 16 ; i++) {
+		altp[j][0] = pontos[indice[i]][0];
+		altp[j][0] = pontos[indice[i]][1];
+		altp[j][0] = pontos[indice[i]][2];
+
+		j++;
+
+		if( j % 4 == 0 ) {
+			ponto = formulaBezier(a,altp[0],altp[1],altp[2],altp[3]);
+			res[x][0] = ponto[0];
+			res[x][1] = ponto[1];
+			res[x][2] = ponto[2];
+
+			k++;
+
+			j = 0;
+		}
+
+	}
+	calculo = formulaBezier(b,res[0],res[1],res[2],res[3]);
+
+	return calculo;
+}
+
+
+void patch( string file , int tess , string name) {
+
+	ofstream fileo(name);
+	string line , aux;
+	ifstream filei(file);
+	int i;
+
+	if(filei.is_open()) {
+		getline(filei,line);
+		int npatch = atoi(line.c_str());
+		int** indices = new int*[npatch];
+		cout << npatch << endl;
+
+		for(int r = 0 ; r < npatch ; r++) {
+			getline(filei,line);
+			indices[r] = new int[16];
+
+			for(int j = 0 ; j < 16 ; j++) {
+				i = line.find(",");
+				aux = line.substr(0,i);
+				indices[r][j] = atoi(aux.c_str());
+				line.erase(0, i + 1);
+			} 
+		}
+
+		getline(filei,line);
+		int npontos = atoi(line.c_str());
+		cout << npontos << endl;
+		float** pontos = new float*[npontos];
+
+		for( int m = 0 ; m < npontos ; m++){
+			getline(filei,line);
+			pontos[m] = new float[3];
+			for( int l = 0 ; l < 3 ; l++) {
+				i = line.find(",");
+				aux = line.substr(0,i);
+				pontos[m][l] = atof(aux.c_str());
+				line.erase(0 , i + 1);
+			}
+		}
+
+		float incre = 1.0 / tess , u , v , u2 , v2;
+		float *** pontoRes = new float**[npatch];
+
+		for(int rr = 0 ; rr < npatch ; rr++) {
+			pontoRes[rr] = new float*[4];
+		 	
+		 	for( int jj = 0 ; jj << tess ; i ++) {
+
+		 		for( int mn = 0 ; mn << tess ; mn++) {
+
+		 			u = jj*incre;
+		 			v = mn*incre;
+		 			u2 = (jj + 1)* incre;
+		 			v2 = (mn + 1)* incre;
+
+		 			pontoRes[rr][0] = bezier(u, v, indices[rr], pontos, npatch, npontos);
+					pontoRes[rr][1] = bezier(u, v2, indices[rr], pontos, npatch, npontos);
+					pontoRes[rr][2] = bezier(u2, v, indices[rr], pontos, npatch, npontos);
+					pontoRes[rr][3] = bezier(u2, v2, indices[rr], pontos, npatch, npontos);
+
+
+					fileo << pontoRes[rr][0][0] << "," << pontoRes[rr][0][1] << "," << pontoRes[rr][0][2] << endl;
+					fileo << pontoRes[rr][2][0] << "," << pontoRes[rr][2][1] << "," << pontoRes[rr][2][2] << endl;
+					fileo << pontoRes[rr][3][0] << "," << pontoRes[rr][3][1] << "," << pontoRes[rr][3][2] << endl;
+
+					fileo << pontoRes[rr][0][0] << "," << pontoRes[rr][0][1] << "," << pontoRes[rr][0][2] << endl;
+					fileo << pontoRes[rr][3][0] << "," << pontoRes[rr][3][1] << "," << pontoRes[rr][3][2] << endl;
+					fileo << pontoRes[rr][1][0] << "," << pontoRes[rr][1][1] << "," << pontoRes[rr][1][2] << endl;
+		 		}
+		 	}
+
+		 }
+	}
+
+	filei.close();
+	fileo.close();
+}
+
+
+
 
 int main (int argc , char **argv){
 
@@ -436,6 +586,9 @@ int main (int argc , char **argv){
 			}
 			else if (strcmp(argv[1],"Cilindro") == 0) {
 				cilindro(atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), atoi(argv[5]), argv[6]);
+			}
+			else if(strcmp(argv[1],"Patch") == 0){
+				patch(argv[2], atoi(argv[3]) , argv[4]);
 			}
 			else {
 				printf("FIGURA IMPOSSÍVEL\n");
